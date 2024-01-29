48c042cb4c76e6c2a5445ec1d9d9badd65532226
User <user@mail.net>
2024-01-29 22:24:42 +0200

    Initial Commit

---
+++ HWS_Animation_learning/HWS_Animation_learningApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_Animation_learningApp.swift
+//  HWS_Animation_learning
+//
+//  Created by Server Admin on 29.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct HWS_Animation_learningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
