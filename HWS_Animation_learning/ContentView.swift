dbb03654dac680ba4dc904606b77b94e4ac3f560
User <user@mail.net>
2024-01-31 18:06:38 +0200

    Creating implicit animations learning

--- HWS_Animation_learning/ContentView.swift
+++ HWS_Animation_learning/ContentView.swift
@@ -8,15 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var animationAmount = 1.0
+    
     var body: some View {
         VStack {
             Button("Tap Me") {
-                // do nothing
+                animationAmount += 1
+                
             }
             .padding(50)
             .background(.red)
             .foregroundStyle(.white)
             .clipShape(Circle())
+            .scaleEffect(animationAmount)
+            .animation(.default, value: animationAmount)
+            .blur(radius: (animationAmount - 1) * 3)
         }
     }
 }
